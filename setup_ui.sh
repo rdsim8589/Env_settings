5ad512f01cbaf54c98c2b90cb7c52f16afc52dab
Richard Sim <rdsim8589@gmail.com>
2017-06-12 20:01:50 -0700

    setting up programs for ubuntu ui

---
+++ setup_ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#install slack
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+sudo apt-get update
+sudo apt-get install -y scudcloud
+
+#install chrome
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+#reminder to install the keybindings to change the key bindings.
