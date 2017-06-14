06b5bdc0c842c80412da41c8377834dd402da897
Richard Sim <rdsim8589@gmail.com>
2017-06-13 19:21:36 -0700

    removed install of scud cloud

--- setup_ui.sh
+++ setup_ui.sh
@@ -3,7 +3,6 @@
 sudo apt-add-repository -y ppa:rael-gc/scudcloud
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get update
-sudo apt-get install -y scudcloud
 
 #install chrome
 sudo apt-get install libxss1 libappindicator1 libindicator7
