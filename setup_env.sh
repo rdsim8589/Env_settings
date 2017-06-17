c75e347b63c0db655207576d3ce43bdde5e1229d
Richard Sim <rdsim8589@gmail.com>
2017-06-17 16:53:12 -0700

    Added installing Go to the set up environment

--- setup_env.sh
+++ setup_env.sh
@@ -4,6 +4,9 @@ sudo apt-get update
 sudo apt-get install -y emacs, vim
 #install git
 sudo apt-get install -y git
+
+#install curl
+sudo apt-get install -y curl
 #setup git config
 git config --global user.email "rdsim8589@gmail.com"
 git config --global user.name "Richard Sim"
@@ -15,3 +18,10 @@ cp .emacs ~/
 #setting up bash aliases
 cp .bash_aliases ~/
 source ~/.bash_aliases
+
+#install Go Lang Version 1.8.3
+curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+tar xvf go1.8.3.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+#current path where go is located
+sudo mv go /usr/local
